fc1e6c84e248b9756b01b977961d431deb33d6e7
Daniel <mydani@gmx.de>
2020-07-05 08:46:21 +0200

    Updated german translation
    Updated german translation
    
    Signed-off-by: Daniel <mydani@gmx.de>

--- debian/makedeb.sh
+++ debian/makedeb.sh
@@ -8,12 +8,12 @@ head -10 changelog
 echo "..."
 echo "================================================="
 echo
-echo "Is changelog up-to-date? (y|n)"
-read ans
-case $ans in
-  y*|Y*) echo Building deb;;
-  *)     echo bye; echo; exit 1;;
-esac
+#echo "Is changelog up-to-date? (y|n)"
+#read ans
+#case $ans in
+#  y*|Y*) echo Building deb;;
+#  *)     echo bye; echo; exit 1;;
+#esac
 # make the debs:
 # -us Do not sign the source package
 # -uc Do not sign the .changes file
